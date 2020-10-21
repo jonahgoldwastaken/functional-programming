08ae791c5f66e6ccf3e0f6aa64c927edcf8d3e52
Jonah Meijers <jonahmeijers97@gmail.com>
2020-10-21 11:28:18 +0200

    fix: change dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { resolve } from 'path'
 
-require('dotenv')({ config: { path: resolve(__dirname, '..', '.env') } })
+require('dotenv').config({ path: resolve(__dirname, '..', '.env') })
 
 console.log('hoi')
