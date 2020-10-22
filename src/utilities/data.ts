82cc496eedc2947545e1845901233a993d589048
Jonah Meijers <jonahmeijers97@gmail.com>
2020-10-22 12:54:24 +0200

    feat: Data keys conversion function

--- src/utilities/data.ts
+++ src/utilities/data.ts
@@ -1,2 +1,16 @@
+interface GenericObject {
+  [key: string]: any
+}
+
 export const pickKeyFromDataset = (key: string) => (dataset: any[]) =>
   [...dataset].map(item => item[key])
+
+export const convertObjectKeys = <obj extends GenericObject>(
+  newKeysObj: obj
+) => (objArray: obj[]) =>
+  [...objArray].map(val =>
+    Object.keys(val)?.reduce(
+      (prev, curr: string) => ({ ...prev, [newKeysObj[curr]]: val[curr] }),
+      {}
+    )
+  )
