3ee7ce5d0de6df74a791988e8c0660a3ca74d0b0
Jonah Meijers <jonahmeijers97@gmail.com>
2020-10-22 12:54:34 +0200

    feat: language helper functions

---
+++ src/helpers/languages.ts
@@ -0,0 +1,42 @@
+import { replaceStringForObjectValue, stringIsInArray } from 'utilities/strings'
+
+const shortenedLanguages = {
+  NL: 'Nederlands',
+  EN: 'Engels',
+  ENG: 'Engels',
+}
+
+/**
+ * List of valid languages pulled from the practice data
+ */
+const validLanguages = [
+  'nederlands',
+  'engels',
+  'duits',
+  'spaans',
+  'frans',
+  'tessels',
+  'berbers',
+  'marokkaans-arabisch',
+  'koreaans',
+  'cantonees',
+  'ghanees',
+  'fries',
+  'kroatisch',
+  'amerikaans',
+  'zweeds',
+  'pools',
+  'tamil',
+  'papiaments',
+  'indonesisch',
+  'gebarentaal',
+  'indisch',
+  'arabisch',
+  'chinees',
+]
+
+export const filterOnValidLanguages = (lang: string) =>
+  stringIsInArray(validLanguages)(lang)
+
+export const mapExpandShortenedLanguage = (lang: string) =>
+  replaceStringForObjectValue(shortenedLanguages)(lang)
