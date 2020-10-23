b0fcd7f66898c86fda9ca6a59709f6624e82f790
Jonah Meijers <jonahmeijers97@gmail.com>
2020-10-23 11:27:44 +0200

    feat: remove all spaces from pet column

--- src/modules/parsePets.ts
+++ src/modules/parsePets.ts
@@ -3,13 +3,18 @@ import { pickKeyFromObject } from '../utilities/object'
 import {
   replaceStringThroughRegex,
   splitStringOnRegex,
+  stringIsValidString,
 } from '../utilities/strings'
 
 const parsePets = <T>(dataset: T[]) =>
   [...dataset]
     .map(pickKeyFromObject('huisDieren'))
     .map(
-      pipe(replaceStringThroughRegex(/\s/, ''), splitStringOnRegex(/-|,|\.|:/))
+      pipe(
+        replaceStringThroughRegex(/\s+/g, ''),
+        splitStringOnRegex(/-|,|\.|:/)
+      )
     )
+    .map(ans => ans.filter(stringIsValidString))
 
 export default parsePets
