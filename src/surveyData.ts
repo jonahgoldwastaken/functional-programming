2e1321989cdbfb82c2ccf922aa950e53694b6ff2
Jonah Meijers <jonahmeijers97@gmail.com>
2020-10-29 19:29:20 +0100

    docs: add comments to surveyData.ts

--- src/surveyData.ts
+++ src/surveyData.ts
@@ -30,33 +30,35 @@ import {
 
 const parseLanguages = (data: GenericObject<string>[]) => {
   const parsedLangauges = pipe(
-    pickKeySplitVals('gesprokenTalen'),
+    pickKeySplitVals('gesprokenTalen'), // Picks the spoken languages from the provided dataset and splits answers into arrays of usable values, each array representing an answer
     map(
+      // Maps these answers
       pipe(
-        filterValidStringsWithFunc(filterStringOnValidLanguages),
-        map(mapExpandCapitaliseLanguage),
-        sort(sortArrayOfStringsAlphabetically)
+        filterValidStringsWithFunc(filterStringOnValidLanguages), // Filters them on validity
+        map(mapExpandCapitaliseLanguage), // Maps every answer, capitalising the first letter
+        sort(sortArrayOfStringsAlphabetically) // Sorts the languages per person alphabetically
       )
     )
   )(data)
-  writeResult(parsedLangauges)('languages-test')
+  writeResult(parsedLangauges)('languages-test') // Writes the result to a file in the 'results' folder name 'languages-test'
 }
 
 const parsePets = (data: GenericObject<string>[]) => {
   const parsedPets = pipe(
-    pickKeySplitVals('huisDieren'),
+    pickKeySplitVals('huisDieren'), // Picks the pets from the provided dataset and splits answers into arrays of usable values, each array representing an answer
     map(
+      // Maps these answers
       pipe(
-        filter(stringMatchesRegEx(/geen/g)),
-        filter(stringEqualsString('N>V>T>')),
-        filter(filterStringLength),
-        reject(arrayValueContainsString(irrelevantValues)),
-        map(pipe(toLower, replaceStringForObjectValue(petLookUpTable))),
-        mapEmptyArraysInArrayToOtherValue('Heeft geen huisdieren'),
+        filter(stringMatchesRegEx(/geen/g)), // Removes the answers saying 'geen'
+        filter(stringEqualsString('N>V>T>')), // Removes one answers euqaling this string
+        filter(filterStringLength), // Filters empty string out of array
+        reject(arrayValueContainsString(irrelevantValues)), // Rejects all strings containing irrelevant values
+        map(pipe(toLower, replaceStringForObjectValue(petLookUpTable))), // Maps these answers, puts the strings in lowercase and replacing relevant values to the values inside the petLookUpTable
+        mapEmptyArraysInArrayToOtherValue('Heeft geen huisdieren'), // Replaces all empty answers to 'Heeft geen huisdieren', stating that people which provided no valid answers
         runFuncIfValIsArr<string, PetData>(
-          petReducer({ amount: [], names: [] })
+          petReducer({ amount: [], names: [] }) // Reduces pets, creating an object with an OccurenceTuple array (amounts) and PetTuples (names) array. These types can be found in the types folder
         ),
-        filterInvalidPetValues
+        filterInvalidPetValues // Filters invalid pet values, cleaning up the data
       )
     )
   )(data)
